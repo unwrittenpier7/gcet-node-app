d492f95f68e37fb0a2f750c3bdc3725228d3336d
unwrittenpier7 <itsmepeter143@gmail.com>
2025-06-03 11:15:39 +0530

    updated

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
 import express from 'express'
 import cors from 'cors'
+import mongoose from 'mongoose'
 
 const app = express();
 app.listen(8080,()=>{
+  mongoose.connect("mongodb://localhost:27017/gcet")
   console.log("Server Started");
 });
+const userSchema =mongoose.Schema({
+  name: {type:string},
+});
+const User = mongoose.model("User", userSchema);
+
 
 app.use(cors());
 
 app.get("/", (req, res)=>{
   return res.send("Good Morning!!");
 });
+
+app.get("/register",async(req,res)=>{
+  const result=await user.insertone({
+    name:"Peter"
+  });
+  res.json(result);
+})
 
 app.get("/greet", (req, res)=>{
   res.send("Greetings!!");
@@ -32,3 +46,4 @@ app.get("/products", (req, res)=>{
   ];
   res.json(products);
 })
+
