6b3775868622bf751ff729993ef3dda5d5d71b16
unwrittenpier7 <itsmepeter143@gmail.com>
2025-06-03 15:21:54 +0530

    updated

--- models/orderModel.js
+++ models/orderModel.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
 
 const orderSchema = mongoose.Schema({
-  email: { type: String, required: true },
-  order: { type: Array, required: true }, 
+  email: { type: String },
+  order: { type: Array },
 });
 
 export default mongoose.model("Order", orderSchema);
